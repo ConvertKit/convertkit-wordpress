4e0a782baacb57a4b8d1903e5bed1d3f6715246f
Travis Northcutt <travis@travisnorthcutt.com>
2019-01-23 14:26:18 -0700

    More WIP testing

--- tests/acceptance/SignInCest.php
+++ tests/acceptance/SignInCest.php
@@ -4,48 +4,63 @@ use Dotenv\Dotenv;
 
 class SignInCest {
 	public function _before( AcceptanceTester $I ) {
+
+		$dotenv = new Dotenv( dirname( dirname( __DIR__ ) ) );
+		$dotenv->load();
+
+		$this->ck_api_key    = getenv( 'CK_API_KEY' );
+		$this->ck_api_secret = getenv( 'CK_API_SECRET' );
+
+
+		$I->cli('plugin activate ConvertKit-WordPress');
+		$I->cli('plugin activate contact-form-7');
+		$I->loginAsAdmin();
 	}
 
 	// tests
 	public function testSettingsPage( AcceptanceTester $I ) {
 
-		$dotenv = new Dotenv( dirname( dirname( __DIR__ ) ) );
-		$dotenv->load();
-
-		$ck_api_key    = getenv( 'CK_API_KEY' );
-		$ck_api_secret = getenv( 'CK_API_SECRET' );
-
-		$I->cli('plugin activate ConvertKit-WordPress');
-		$I->cli('plugin activate contact-form-7');
-
 		$I->loginAsAdmin();
 		$I->amOnPluginsPage();
-		$I->seePluginActivated( 'convertkit' );
-		$I->seePluginActivated( 'contact-form-7' );
-		echo 'fart';
-		codecept_debug('fart');
-		global $wpdb;
-		codecept_debug(print_r($wpdb, true));
-		die;
-
 
 		$I->amOnPage( '/wp-admin/options-general.php?page=_wp_convertkit_settings' );
-		$I->fillField( "//input[@name='_wp_convertkit_settings[api_key]']", $ck_api_key );
-		$I->fillField( "//input[@name='_wp_convertkit_settings[api_secret]']", $ck_api_secret );
+		$I->fillField( "//input[@name='_wp_convertkit_settings[api_key]']", $this->ck_api_key );
+		$I->fillField( "//input[@name='_wp_convertkit_settings[api_secret]']", $this->ck_api_secret );
+		$I->click( 'Save Changes' );
+		$I->seeElement( 'option', [ 'value' => 'default' ] );
+		$I->seeElement( 'option', [ 'value' => '820085' ] );
+
+		$I->click( 'Contact Form 7' );
+		$I->see( 'ConvertKit Form' );
+		$I->seeElement( 'option', [ 'value' => 'default' ] );
+		$I->seeElement( 'option', [ 'value' => '820085' ] );
+
+		$I->selectOption( 'form select[id=_wp_convertkit_integration_contactform7_settings_5]', 'Clean form' );
+		$I->click( 'Save Changes' );
+
+		$I->seeOptionIsSelected( 'form select[id=_wp_convertkit_integration_contactform7_settings_5]', 'Clean form' );
+	}
+
+	public function testJavascriptNotLoaded( AcceptanceTester $I ) {
+
+		$I->amOnPage( '/wp-admin/options-general.php?page=_wp_convertkit_settings' );
+		$I->checkOption('#debug');
+		$I->checkOption('#no_scripts');
 		$I->click('Save Changes');
-		$I->seeElement('option', ['value' => 'default']);
-		$I->seeElement('option', ['value' => '820085']);
 
-		$I->click('Contact Form 7');
-		$I->see('ConvertKit Form');
-		$I->seeElement('option', ['value' => 'default']);
-		$I->seeElement('option', ['value' => '820085']);
+		$I->amOnPage('/');
 
-		$I->seeOptionIsSelected('form select[id=_wp_convertkit_integration_contactform7_settings_5]', 'None');
+		$I->dontSeeInSource('wp-convertkit.js');
+	}
 
-		$I->selectOption('form select[id=_wp_convertkit_integration_contactform7_settings_5]', 'Clean form');
+	public function testJavascriptLoaded( AcceptanceTester $I ) {
+
+		$I->amOnPage( '/wp-admin/options-general.php?page=_wp_convertkit_settings' );
+		$I->uncheckOption('#no_scripts');
 		$I->click('Save Changes');
 
-		$I->seeOptionIsSelected('form select[id=_wp_convertkit_integration_contactform7_settings_5]', 'Clean form');
+		$I->amOnPage('/');
+
+		$I->seeInSource('wp-convertkit.js');
 	}
 }
