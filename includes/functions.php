49cde6c174c94478699d98a0bb8c2b30ed0fdea0
George Gecewicz <875201+ggwicz@users.noreply.github.com>
2020-11-10 10:49:14 -0500

    fix: use loose truthy comparison for convert_wp_debug_enabled() check

--- includes/functions.php
+++ includes/functions.php
@@ -14,5 +14,5 @@
 function convertkit_wp_debug_enabled() {
 	$options = get_option( WP_ConvertKit::SETTINGS_PAGE_SLUG );
 
-	return ! empty( $options['debug'] ) && true === $options['debug'];
+	return ! empty( $options['debug'] ) && true == $options['debug']; // phpcs:ignore -- Okay use of loose comparison.
 }
