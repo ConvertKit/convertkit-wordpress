1de30f78ef41bc0a9c1e44c683f3ec2ca13a79b2
George Gecewicz <875201+ggwicz@users.noreply.github.com>
2021-01-12 19:10:51 -0500

    add new convertkit_wp_get_user_agent() function for getting custom user-agent string

--- includes/functions.php
+++ includes/functions.php
@@ -16,3 +16,22 @@ function convertkit_wp_debug_enabled() {
 
 	return ! empty( $options['debug'] ) && true == $options['debug']; // phpcs:ignore -- Okay use of loose comparison.
 }
+
+/**
+ * Gets a customized version of the WordPress default user agent; includes WP Version, PHP version, and ConvertKit plugin version.
+ *
+ * @return string
+ */
+function convertkit_wp_get_user_agent() {
+
+	// Include an unmodified $wp_version.
+	require ABSPATH . WPINC . '/version.php';
+
+	return sprintf(
+		'WordPress/%1$s;PHP/%2$s;ConvertKit/%3$s;%4$s',
+		$wp_version,
+		phpversion(),
+		CONVERTKIT_PLUGIN_VERSION,
+		home_url( '/' )
+	);
+}
