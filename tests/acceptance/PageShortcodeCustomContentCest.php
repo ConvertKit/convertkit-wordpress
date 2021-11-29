8359b825fab094cab30eba6406349e4b09621e84
Tim Carr <tim@n7studios.co.uk>
2021-11-29 16:39:58 -0600

    Add command to test to fix Plugin folder permissions before running tests

--- tests/acceptance/PageShortcodeCustomContentCest.php
+++ tests/acceptance/PageShortcodeCustomContentCest.php
@@ -21,41 +21,6 @@ class PageShortcodeCustomContentCest
 
 	/**
 	 * Test the [convertkit_content] shortcode works when a valid Tag ID is specified,
-	 * and a valid Subscriber ID is used who is subscribed to the tag.
-	 * 
-	 * @since 	1.9.6
-	 * 
-	 * @param 	AcceptanceTester 	$I 	Tester
-	 */
-	public function testFormShortcodeWithValidTagParameterAndValidSubscriberID(AcceptanceTester $I)
-	{
-		// Create Page with Shortcode.
-		$I->havePageInDatabase([
-			'post_name' 	=> 'convertkit-custom-content-shortcode-valid-tag-param-and-valid-subscriber-id',
-			'post_content'	=> '[convertkit_content tag="' . $_ENV['CONVERTKIT_API_TAG_ID'] . '"]ConvertKitCustomContent[/convertkit_content]',
-		]);
-
-		// Load the Page on the frontend site.
-		$I->amOnPage('/convertkit-custom-content-shortcode-valid-tag-param-and-valid-subscriber-id');
-
-		// Check that no PHP warnings or notices were output.
-		$I->checkNoWarningsAndNoticesOnScreen($I);
-
-		// Confirm that the Custom Content is not yet displayed.
-		$I->dontSeeInSource('ConvertKitCustomContent');
-
-		// Reload the page, this time with a subscriber ID who is already subscribed to the tag.
-		$I->amOnPage('/convertkit-custom-content-shortcode-valid-tag-param-and-valid-subscriber-id?ck_subscriber_id=' . $_ENV['CONVERTKIT_API_SUBSCRIBER_ID']);
-
-		// Check that no PHP warnings or notices were output.
-		$I->checkNoWarningsAndNoticesOnScreen($I);
-
-		// Confirm that the Custom Content is now displayed.
-		$I->seeInSource('ConvertKitCustomContent');
-	}
-
-	/**
-	 * Test the [convertkit_content] shortcode works when a valid Tag ID is specified,
 	 * and an invalid Subscriber ID is used.
 	 * 
 	 * @since 	1.9.6
@@ -87,5 +52,40 @@ class PageShortcodeCustomContentCest
 
 		// Confirm that the Custom Content is not yet displayed.
 		$I->dontSeeInSource('ConvertKitCustomContent');
+	}
+
+	/**
+	 * Test the [convertkit_content] shortcode works when a valid Tag ID is specified,
+	 * and a valid Subscriber ID is used who is subscribed to the tag.
+	 * 
+	 * @since 	1.9.6
+	 * 
+	 * @param 	AcceptanceTester 	$I 	Tester
+	 */
+	public function testFormShortcodeWithValidTagParameterAndValidSubscriberID(AcceptanceTester $I)
+	{
+		// Create Page with Shortcode.
+		$I->havePageInDatabase([
+			'post_name' 	=> 'convertkit-custom-content-shortcode-valid-tag-param-and-valid-subscriber-id',
+			'post_content'	=> '[convertkit_content tag="' . $_ENV['CONVERTKIT_API_TAG_ID'] . '"]ConvertKitCustomContent[/convertkit_content]',
+		]);
+
+		// Load the Page on the frontend site.
+		$I->amOnPage('/convertkit-custom-content-shortcode-valid-tag-param-and-valid-subscriber-id');
+
+		// Check that no PHP warnings or notices were output.
+		$I->checkNoWarningsAndNoticesOnScreen($I);
+
+		// Confirm that the Custom Content is not yet displayed.
+		$I->dontSeeInSource('ConvertKitCustomContent');
+
+		// Reload the page, this time with a subscriber ID who is already subscribed to the tag.
+		$I->amOnPage('/convertkit-custom-content-shortcode-valid-tag-param-and-valid-subscriber-id?ck_subscriber_id=' . $_ENV['CONVERTKIT_API_SUBSCRIBER_ID']);
+
+		// Check that no PHP warnings or notices were output.
+		$I->checkNoWarningsAndNoticesOnScreen($I);
+
+		// Confirm that the Custom Content is now displayed.
+		$I->seeInSource('ConvertKitCustomContent');
 	}
 }
