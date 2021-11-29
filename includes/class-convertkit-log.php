08420ef527d566c5df4237128f7e523dd1361559
Tim Carr <tim@n7studios.co.uk>
2021-11-29 16:47:17 -0600

    Changed log name to convertkit-log.txt

--- includes/class-convertkit-log.php
+++ includes/class-convertkit-log.php
@@ -30,7 +30,7 @@ class ConvertKit_Log {
 	public function __construct() {
 
 		// Define location of log file.
-		$this->log_file = WP_CONTENT_DIR . 'log.txt';
+		$this->log_file = trailingslashit( WP_CONTENT_DIR ) . 'convertkit-log.txt';
 
 		// Initialize WP_Filesystem.
 		require_once ABSPATH . 'wp-admin/includes/file.php';
