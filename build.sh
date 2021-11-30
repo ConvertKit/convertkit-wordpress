8431e61b0ad568d4bc5a2ae26feec377c490f096
Tim Carr <tim@n7studios.co.uk>
2021-11-29 18:32:48 -0600

    Adding var_dump() for suppressed PHP errors on failing tests

--- build.sh
+++ build.sh
@@ -1,2 +1,3 @@
 # Creates a Plugin ZIP File of the current code, useful for installing on test sites
+rm convertkit.zip
 zip -r convertkit.zip . -x "*.git*" -x ".wordpress-org/*" -x "tests/*" -x "vendor/*" -x "*.distignore" -x "*.env.*" -x "*codeception.*" -x "composer.json" -x "composer.lock" -x "*.md" -x "log.txt" -x "package-lock.json" -x "package.json" -x "phpcs.xml" -x "*.DS_Store"
