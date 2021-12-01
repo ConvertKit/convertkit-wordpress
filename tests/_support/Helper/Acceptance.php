b756c6a97f4b4e7d6f859a4929fa669f43276aeb
Tim Carr <tim@n7studios.co.uk>
2021-12-01 13:46:52 -0600

    Testing: Added Tests for WishList Member Integration

--- tests/_support/Helper/Acceptance.php
+++ tests/_support/Helper/Acceptance.php
@@ -1,6 +1,8 @@
 <?php
 namespace Helper;
 
+use \Facebook\WebDriver\WebDriverElement;
+
 // here you can define custom actions
 // all public methods declared in helper class will be available in $I
 
@@ -203,5 +205,100 @@ class Acceptance extends \Codeception\Module
 
 		// Check that no PHP warnings or notices were output.
 		$I->checkNoWarningsAndNoticesOnScreen($I);
+	}
+
+	/**
+	 * Helper method to enable the Plugin's Settings > General > Debug option.
+	 * 
+	 * @since 	1.9.6
+	 */
+	public function enableDebugLog($I)
+	{
+		// Go to the Plugin's Settings Screen.
+		$I->loadConvertKitSettingsGeneralScreen($I);
+		
+		// Tick field.
+		$I->checkOption('#debug');
+
+		// Click the Save Changes button.
+		$I->click('Save Changes');
+	}
+
+	/**
+	 * Helper method to determine if the given entry exists in the Plugin Debug Log screen's textarea.
+	 * 
+	 * @since 	1.9.6
+	 */
+	public function seeInPluginDebugLog($I, $entry)
+	{
+		$I->loadConvertKitSettingsToolsScreen($I);
+		$I->seeInSource($entry);
+	}
+
+	/**
+	 * Helper method to determine if the given entry does not exist in the Plugin Debug Log screen's textarea.
+	 * 
+	 * @since 	1.9.6
+	 */
+	public function dontSeeInPluginDebugLog($I, $entry)
+	{
+		$I->loadConvertKitSettingsToolsScreen($I);
+		$I->dontSeeInSource($entry);
+	}
+
+	/**
+	 * Helper method to activate the WishList Member Plugin.
+	 * 
+	 * @since 	1.9.6
+	 */
+	public function activateWishListMemberPlugin($I)
+	{
+		// Login as the Administrator
+		$I->loginAsAdmin();
+
+		// Go to the Plugins screen in the WordPress Administration interface.
+		$I->amOnPluginsPage();
+
+		// Activate the Plugin.
+		$I->activatePlugin('wishlist-member');
+
+		// Check that the Plugin activated successfully.
+		$I->seePluginActivated('wishlist-member');
+
+		// Check that no PHP warnings or notices were output.
+		$I->checkNoWarningsAndNoticesOnScreen($I);
+	}
+
+	/**
+	 * Helper method to setup the WishList Member Plugin.
+	 * 
+	 * @since 	1.9.6
+	 */
+	public function setupWishListMemberPlugin($I)
+	{
+		// Load WishList Member Settings screen, which will load the first time configuration wizard.
+		$I->amOnAdminPage('admin.php?page=WishListMember');
+
+		// Step 1
+		$I->fillField('input[name="name"]', 'Bronze');
+
+		$I->click('.step-1 a[next-screen="step-2"]');
+
+		// Step 2
+		$I->click('.step-2 a[next-screen="step-3"]');
+
+		// Step 3
+		$I->click('.step-3 a[next-screen="step-4"]');
+
+		// Step 4
+		$I->click('.step-4 a[next-screen="step-5"]');
+
+		// Save
+		$I->click('.step-5 a.save-btn');
+
+		$I->performOn('.-congrats-gs', function($I) {
+			$I->click('a.next-btn');
+			$I->seeInSource('Bronze');
+		});
 	}
 }
