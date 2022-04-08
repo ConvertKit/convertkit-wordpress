04a12273a1cec0b13b160372458baa936f07ea59
Tim Carr <tim@n7studios.co.uk>
2022-04-08 14:25:47 +0100

    Ignore PHPStan configuration files when building Plugin ZIP

--- .scripts/build.sh
+++ .scripts/build.sh
@@ -6,4 +6,4 @@ php -n $(which wp) i18n make-pot ../ ../languages/convertkit.pot
 
 # Build ZIP file
 rm ../convertkit.zip
-cd .. && zip -r convertkit.zip . -x "*.git*" -x ".scripts/*" -x ".wordpress-org/*" -x "tests/*" -x "vendor/*" -x "*.distignore" -x "*.env.*" -x "*codeception.*" -x "composer.json" -x "composer.lock" -x "*.md" -x "log.txt" -x "package-lock.json" -x "package.json" -x "phpcs.xml" -x "phpstan.neon" -x "phpstan.neon.dist" -x "phpstan.neon.example" -x "*.DS_Store"
+cd .. && zip -r convertkit.zip . -x "*.git*" -x ".scripts/*" -x ".wordpress-org/*" -x "tests/*" -x "vendor/*" -x "*.distignore" -x "*.env.*" -x "*codeception.*" -x "composer.json" -x "composer.lock" -x "*.md" -x "log.txt" -x "phpcs.xml" -x "phpstan.neon" -x "phpstan.neon.dist" -x "phpstan.neon.example" -x "*.DS_Store"
