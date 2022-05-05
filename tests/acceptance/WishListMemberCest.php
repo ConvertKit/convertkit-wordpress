1b96b2de52b3cdd78aa096246d42a58ff6e93bc4
Tim Carr <tim@n7studios.co.uk>
2022-05-05 18:13:27 +0100

    Merge branch 'master' into testing-refactoring

--- tests/acceptance/WishListMemberCest.php
+++ tests/acceptance/WishListMemberCest.php
@@ -120,15 +120,16 @@ class WishListMemberCest
 	 */
 	private function _setupWishListMemberPlugin($I)
 	{
+		// Prevent WishList Member from asking for a license key by populating the WLM options table.
+		$I->updateInDatabase($I->grabPrefixedTableNameFor('wlm_options'), [
+			'option_value' => '1',
+		], [
+			'option_name' => 'LicenseStatus',
+		]);
+
 		// Load WishList Member Settings screen, which will load the first time configuration wizard.
 		$I->amOnAdminPage('admin.php?page=WishListMember');
 
-		// Skip Licensing
-		$I->click('a.skip-license');
-		$I->performOn( 'a[next-screen="start"]', function($I) {
-			$I->click('a[next-screen="start"]');
-		});
-		
 		// Step 1
 		$I->fillField('input[name="name"]', 'Bronze');
 		$I->click('.step-1 a[next-screen="step-2"]');
