fbb0095051c553b39fbc806acc84e5fa4ae3ecff
Tim Carr <tim@n7studios.co.uk>
2022-05-06 15:13:24 +0100

    Refactored Page Tag tests

--- tests/acceptance/PageTagCest.php
+++ tests/acceptance/PageTagCest.php
@@ -19,13 +19,6 @@ class PageTagCest
 		$I->activateConvertKitPlugin($I);
 		$I->setupConvertKitPlugin($I);
 		$I->enableDebugLog($I);
-		$I->wait(2);
-
-		// Navigate to Pages > Add New
-		$I->amOnAdminPage('post-new.php?post_type=page');
-
-		// Close the Gutenberg "Welcome to the block editor" dialog if it's displayed
-		$I->maybeCloseGutenbergWelcomeModal($I);
 	}
 
 	/**
@@ -38,42 +31,16 @@ class PageTagCest
 	 */
 	public function testAddNewPageUsingNoTag(AcceptanceTester $I)
 	{
-		// Navigate to Pages > Add New
-		$I->amOnAdminPage('post-new.php?post_type=page');
+		// Add a Page using the Gutenberg editor.
+		$I->addGutenbergPage($I, 'page', 'ConvertKit: Page: Tag: None');
 
-		// Check that no PHP warnings or notices were output.
-		$I->checkNoWarningsAndNoticesOnScreen($I);
+		// Configure metabox's Tag setting = None.
+		$I->configureMetaboxSettings($I, 'wp-convertkit-meta-box', [
+			'tag' => [ 'select2', 'None' ],
+		]);
 
-		// Check that the metabox is displayed.
-		$I->seeElementInDOM('#wp-convertkit-meta-box');
-
-		// Check that the Form option is displayed.
-		$I->seeElementInDOM('#wp-convertkit-tag');
-
-		// Change Tag to 'None'
-		$I->fillSelect2Field($I, '#select2-wp-convertkit-tag-container', 'None');
-
-		// Define a Page Title.
-		$I->fillField('.editor-post-title__input', 'ConvertKit: Tag: None');
-
-		// Click the Publish button.
-		$I->click('.editor-post-publish-button__button');
-
-		// When the pre-publish panel displays, click Publish again.
-		$I->performOn('.editor-post-publish-panel__prepublish', function($I) {
-			$I->click('.editor-post-publish-panel__header-publish-button .editor-post-publish-button__button');	
-		});
-
-		// Check the value of the Tag field matches the input provided.
-		$I->performOn( '.post-publish-panel__postpublish-buttons', function($I) {
-			$I->seeOptionIsSelected('#wp-convertkit-tag', 'None');
-		});
-
-		// Load the Page on the frontend site.
-		$I->amOnPage('/convertkit-tag-none');
-
-		// Check that no PHP warnings or notices were output.
-		$I->checkNoWarningsAndNoticesOnScreen($I);
+		// Publish and view the Page on the frontend site.
+		$I->publishAndViewGutenbergPage($I);
 
 		// Confirm that the tag parameter is not set to the Tag ID.
 		$I->dontSeeInSource('"tag":"' . $_ENV['CONVERTKIT_API_TAG_ID'] . '"');
@@ -89,48 +56,25 @@ class PageTagCest
 	 */
 	public function testAddNewPageUsingDefinedTag(AcceptanceTester $I)
 	{
-		// Navigate to Pages > Add New
-		$I->amOnAdminPage('post-new.php?post_type=page');
+		// Add a Page using the Gutenberg editor.
+		$I->addGutenbergPage($I, 'page', 'ConvertKit: Page: Tag: ' . $_ENV['CONVERTKIT_API_TAG_NAME'] );
 
-		// Check that no PHP warnings or notices were output.
-		$I->checkNoWarningsAndNoticesOnScreen($I);
-
-		// Check that the metabox is displayed.
-		$I->seeElementInDOM('#wp-convertkit-meta-box');
-
-		// Check that the Form option is displayed.
-		$I->seeElementInDOM('#wp-convertkit-tag');
-
-		// Change Tag to value specified in the .env file.
-		$I->fillSelect2Field($I, '#select2-wp-convertkit-tag-container', $_ENV['CONVERTKIT_API_TAG_NAME']);
-
-		// Define a Page Title.
-		$I->fillField('.editor-post-title__input', 'ConvertKit: Tag: Specific');
-
-		// Click the Publish button.
-		$I->click('.editor-post-publish-button__button');
-		
-		// When the pre-publish panel displays, click Publish again.
-		$I->performOn('.editor-post-publish-panel__prepublish', function($I) {
-			$I->click('.editor-post-publish-panel__header-publish-button .editor-post-publish-button__button');	
-		});
-
-		// Check the value of the Tag field matches the input provided.
-		$I->performOn( '.post-publish-panel__postpublish-buttons', function($I) {
-			$I->seeOptionIsSelected('#wp-convertkit-tag', $_ENV['CONVERTKIT_API_TAG_NAME']);
-		});
+		// Configure metabox's Tag setting to the value specified in the .env file.
+		$I->configureMetaboxSettings($I, 'wp-convertkit-meta-box', [
+			'tag' => [ 'select2', $_ENV['CONVERTKIT_API_TAG_NAME'] ],
+		]);
 
 		// Get Tag ID.
 		$tagID = $I->grabValueFrom('#wp-convertkit-tag');
 
-		// Load the Page on the frontend site
-		$I->amOnPage('/convertkit-tag-specific');
+		// Confirm it matches the Tag ID in the .env file.
+		$I->assertEquals($tagID, $_ENV['CONVERTKIT_API_TAG_ID']);
 
-		// Check that no PHP warnings or notices were output.
-		$I->checkNoWarningsAndNoticesOnScreen($I);
+		// Publish and view the Page on the frontend site.
+		$I->publishAndViewGutenbergPage($I);
 
 		// Confirm that the post_has_tag parameter is set to true in the source code.
-		$I->seeInSource('"tag":"' . $_ENV['CONVERTKIT_API_TAG_ID'] . '"');
+		$I->seeInSource('"tag":"' . $tagID . '"');
 	}
 
 	/**
