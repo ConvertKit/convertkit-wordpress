40d2962ec5a7cd5622605c60b11636bf43ce3cea
Tim Carr <tim@n7studios.co.uk>
2022-05-25 11:41:39 +0100

    Improved reliability of WooCommerce tests

--- tests/acceptance/IntegrationWooCommerceProductFormCest.php
+++ tests/acceptance/IntegrationWooCommerceProductFormCest.php
@@ -35,6 +35,9 @@ class IntegrationWooCommerceProductFormCest
 		// Add a Product using the Classic Editor.
 		$I->addClassicEditorPage($I, 'product', 'ConvertKit: Product: Form: Default: None');
 
+		// Wait, otherwise publishing will fail in WooCommerce.
+		$I->wait(1);
+
 		// Publish and view the Page on the frontend site.
 		$I->publishAndViewClassicEditorPage($I);
 
@@ -58,6 +61,9 @@ class IntegrationWooCommerceProductFormCest
 		// Add a Product using the Classic Editor.
 		$I->addClassicEditorPage($I, 'product', 'ConvertKit: Product: Form: Default');
 
+		// Wait, otherwise publishing will fail in WooCommerce.
+		$I->wait(1);
+
 		// Publish and view the Page on the frontend site.
 		$I->publishAndViewClassicEditorPage($I);
 
@@ -75,16 +81,14 @@ class IntegrationWooCommerceProductFormCest
 	 */
 	public function testAddNewProductUsingNoForm(AcceptanceTester $I)
 	{
-		// Navigate to Products > Add New.
-		// Don't use addClassicEditorPage(); on WooCommerce Products, it results in the Publish button no longer working
-		// for some inexplicible reason.
-		$I->amOnAdminPage('post-new.php?post_type=product');
+		// Create Product.
+		$I->addClassicEditorPage($I, 'product', 'ConvertKit: Product: Form: None');
 
 		// Change Form to None.
 		$I->fillSelect2Field($I, '#select2-wp-convertkit-form-container', 'None', 'aria-owns');
 
-		// Define a Product Title.
-		$I->fillField('#title', 'ConvertKit: Product: Form: None');
+		// Wait, otherwise publishing will fail in WooCommerce.
+		$I->wait(1);
 
 		// Publish and view the Product.
 		$I->publishAndViewClassicEditorPage($I);
@@ -103,19 +107,14 @@ class IntegrationWooCommerceProductFormCest
 	 */
 	public function testAddNewProductUsingDefinedForm(AcceptanceTester $I)
 	{
-		// Navigate to Products > Add New.
-		// Don't use addClassicEditorPage(); on WooCommerce Products, it results in the Publish button no longer working
-		// for some inexplicible reason.
-		$I->amOnAdminPage('post-new.php?post_type=product');
-
-		// Define a Product Title.
-		$I->fillField('#title', 'ConvertKit: Product: Form: Defined');
+		// Create Product.
+		$I->addClassicEditorPage($I, 'product', 'ConvertKit: Product: Form: Defined');
 
 		// Change Form to Form setting in .env file.
 		$I->fillSelect2Field($I, '#select2-wp-convertkit-form-container', $_ENV['CONVERTKIT_API_FORM_NAME'], 'aria-owns');
 
-		// Define a Product Title.
-		$I->fillField('#title', 'ConvertKit: Product: Form: Defined');
+		// Wait, otherwise publishing will fail in WooCommerce.
+		$I->wait(1);
 
 		// Publish and view the Product.
 		$I->publishAndViewClassicEditorPage($I);
