1fd830c9fc707b68e2550c2b59556eda0bab8522
Tim Carr <tim@n7studios.co.uk>
2022-08-09 18:07:17 +0100

    Test: Revert testing to original specification

--- tests/acceptance/widgets/WidgetBroadcastsCest.php
+++ tests/acceptance/widgets/WidgetBroadcastsCest.php
@@ -26,6 +26,82 @@ class WidgetBroadcastsCest
 	}
 
 	/**
+	 * Test the Broadcasts block works when using the default parameters.
+	 * 
+	 * @since 	1.9.8.2
+	 * 
+	 * @param 	AcceptanceTester 	$I 	Tester
+	 */
+	public function testBroadcastsBlockWidgetWithDefaultParameters(AcceptanceTester $I)
+	{
+		// Add block widget.
+		$I->addBlockWidget($I, 'ConvertKit Broadcasts', 'convertkit-broadcasts');
+
+		// View the home page.
+		$I->amOnPage('/');
+
+		// Confirm that the block displays.
+		$I->seeBroadcastsOutput($I);
+
+		// Confirm that the default date format is as expected.
+		$I->seeInSource('<time datetime="2022-04-08">April 8, 2022</time>');
+
+		// Confirm that the default expected number of Broadcasts are displayed.
+		$I->seeNumberOfElements('li.convertkit-broadcast', [1,10]);
+	}
+
+	/**
+	 * Test the Broadcasts block's date format parameter works.
+	 * 
+	 * @since 	1.9.8.2
+	 * 
+	 * @param 	AcceptanceTester 	$I 	Tester
+	 */
+	public function testBroadcastsBlockWidgetWithDateFormatParameter(AcceptanceTester $I)
+	{
+		// Add block widget.
+		$I->addBlockWidget($I, 'ConvertKit Broadcasts', 'convertkit-broadcasts', [
+			'date_format' => [ 'select', 'Y-m-d' ],
+		]);
+
+		// View the home page.
+		$I->amOnPage('/');
+
+		// Confirm that the block displays.
+		$I->seeBroadcastsOutput($I);
+
+		// Confirm that the date format is as expected.
+		$I->seeInSource('<time datetime="2022-04-08">2022-04-08</time>');
+
+		// Confirm that the default expected number of Broadcasts are displayed.
+		$I->seeNumberOfElements('li.convertkit-broadcast', [1,10]);
+	}
+
+	/**
+	 * Test the Broadcasts block's limit parameter works.
+	 * 
+	 * @since 	1.9.8.2
+	 * 
+	 * @param 	AcceptanceTester 	$I 	Tester
+	 */
+	public function testBroadcastsBlockWidgetWithLimitParameter(AcceptanceTester $I)
+	{
+		// Add block widget.
+		$I->addBlockWidget($I, 'ConvertKit Broadcasts', 'convertkit-broadcasts', [
+			'limit' => [ 'input', '2' ],
+		]);
+
+		// View the home page.
+		$I->amOnPage('/');
+
+		// Confirm that the block displays.
+		$I->seeBroadcastsOutput($I);
+
+		// Confirm that the expected number of Broadcasts are displayed.
+		$I->seeNumberOfElements('li.convertkit-broadcast', 2);
+	}
+
+	/**
 	 * Test the Broadcasts block's pagination works when enabled.
 	 * 
 	 * @since 	1.9.8.2
@@ -36,15 +112,42 @@ class WidgetBroadcastsCest
 	{
 		// Add block widget.
 		$I->addBlockWidget($I, 'ConvertKit Broadcasts', 'convertkit-broadcasts', [
-			'limit' 					=> [ 'input', '1' ],
 			'.components-form-toggle' 	=> [ 'toggle', true ],
+			'limit' 	 		  		=> [ 'input', '1' ],
+		]);
+
+		// View the home page.
+		$I->amOnPage('/');
+
+		// Confirm that the block displays.
+		$I->seeBroadcastsOutput($I);
+
+		// Confirm that the expected number of Broadcasts are displayed.
+		$I->seeNumberOfElements('li.convertkit-broadcast', 2);
+	}
+
+	/**
+	 * Test the Broadcasts block's pagination labels work when defined.
+	 * 
+	 * @since 	1.9.8.2
+	 * 
+	 * @param 	AcceptanceTester 	$I 	Tester
+	 */
+	public function testBroadcastsBlockWithPaginationLabelParameters(AcceptanceTester $I)
+	{
+		// Add block widget.
+		$I->addBlockWidget($I, 'ConvertKit Broadcasts', 'convertkit-broadcasts', [
+			'.components-form-toggle' 	=> [ 'toggle', true ],
+			'limit' 	 		  		=> [ 'input', '1' ],
+			'paginate_label_prev' 		=> [ 'input', 'Newer' ],
+			'paginate_label_next' 		=> [ 'input', 'Older' ],
 		]);
 
 		// View the home page.
 		$I->amOnPage('/');
 
 		// Test pagination.
-		$I->testBroadcastsPagination($I, 'Previous', 'Next');
+		$I->testBroadcastsPagination($I, 'Older', 'Newer');
 	}
 
 	/**
