ee9752bad52aea171bffbfff1300a35a533f988f
Tim Carr <tim@n7studios.co.uk>
2022-09-08 17:42:26 +0100

    Products Resource function to return button HTML

--- includes/class-convertkit-resource-products.php
+++ includes/class-convertkit-resource-products.php
@@ -50,4 +50,49 @@ class ConvertKit_Resource_Products extends ConvertKit_Resource {
 
 	}
 
+	/**
+	 * Returns the HTML button markup for the given Product ID.
+	 *
+	 * @since   1.9.8.5
+	 *
+	 * @param   int    $id             Product ID.
+	 * @param   string $button_text    Button Text.
+	 * @param   array  $css_classes    CSS classes to apply to link (typically included when using Gutenberg).
+	 * @param   array  $css_styles     CSS inline styles to apply to link (typically included when using Gutenberg).
+	 * @return  WP_Error|string         Button HTML
+	 */
+	public function get_html( $id, $button_text, $css_classes = array(), $css_styles = array() ) {
+
+		// Cast ID to integer.
+		$id = absint( $id );
+
+		// Bail if the resources are a WP_Error.
+		if ( is_wp_error( $this->resources ) ) {
+			return $this->resources;
+		}
+
+		// Bail if the resource doesn't exist.
+		if ( ! isset( $this->resources[ $id ] ) ) {
+			return new WP_Error(
+				'convertkit_resource_products_get_html',
+				sprintf(
+					/* translators: ConvertKit Product ID */
+					__( 'ConvertKit Product ID %s does not exist on ConvertKit.', 'convertkit' ),
+					$id
+				)
+			);
+		}
+
+		// Build button HTML.
+		$html  = '<div class="wp-block-button">';
+		$html .= '<a href="' . $this->resources[ $id ]['url'] . '" class="wp-block-button__link ' . esc_attr( implode( ' ', $css_classes ) ) . '" style="' . implode( ';', $css_styles ) . '" data-commerce>';
+		$html .= esc_html( $button_text );
+		$html .= '</a>';
+		$html .= '</div>';
+
+		// Return.
+		return $html;
+
+	}
+
 }
