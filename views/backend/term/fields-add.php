229c4fcad9be67f60dc447e5999fbd792b39b2ab
Tim Carr <tim@n7studios.co.uk>
2022-12-01 13:38:19 +0000

    Match UI of setting with edit category

--- views/backend/term/fields-add.php
+++ views/backend/term/fields-add.php
@@ -10,7 +10,7 @@
 <div class="form-field term-description-wrap">
 	<label for="tag-description"><?php esc_html_e( 'ConvertKit Form', 'convertkit' ); ?></label>
 
-	<div class="convertkit-select2-container">
+	<div class="convertkit-select2-container convertkit-select2-container-grid">
 		<select name="wp-convertkit[form]" id="wp-convertkit-form" class="convertkit-select2">
 			<option value="0" data-preserve-on-refresh="1" selected>
 				<?php esc_html_e( 'Default', 'convertkit' ); ?>
@@ -30,19 +30,18 @@
 		<button class="wp-convertkit-refresh-resources" class="button button-secondary" title="<?php esc_attr_e( 'Refresh Forms from ConvertKit account', 'convertkit' ); ?>" data-resource="forms" data-field="#wp-convertkit-form">
 			<span class="dashicons dashicons-update"></span>
 		</button>
+		<p class="description">
+			<?php _e( '<code>Default</code>: Display a form based on the Post\'s settings.', 'convertkit' ); /* phpcs:ignore */ ?>
+			<br />
+			<?php
+			echo sprintf(
+				/* translators: Taxonomy Name */
+				esc_html__( 'Any other option will display that form after the main content for Posts assigned to this %s.', 'convertkit' ),
+				'category'
+			);
+			?>
+		</p>
 	</div>
-
-	<p class="description">
-		<?php _e( '<code>Default</code>: Display a form based on the Post\'s settings.', 'convertkit' ); /* phpcs:ignore */ ?>
-		<br />
-		<?php
-		echo sprintf(
-			/* translators: Taxonomy Name */
-			esc_html__( 'Any other option will display that form after the main content for Posts assigned to this %s.', 'convertkit' ),
-			'category'
-		);
-		?>
-	</p>
 
 	<?php
 	wp_nonce_field( 'wp-convertkit-save-meta', 'wp-convertkit-save-meta-nonce' );
