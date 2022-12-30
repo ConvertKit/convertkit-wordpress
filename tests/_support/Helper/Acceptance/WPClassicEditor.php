d7aec9b95bcdfb15e282b708c51f894047a2fb66
Tim Carr <tim@n7studios.co.uk>
2022-12-30 19:42:44 +0000

    Tests: WooCommerce: Scroll above product description to fix failing test

--- tests/_support/Helper/Acceptance/WPClassicEditor.php
+++ tests/_support/Helper/Acceptance/WPClassicEditor.php
@@ -116,7 +116,7 @@ class WPClassicEditor extends \Codeception\Module
 		// Scroll to the applicable TinyMCE editor.
 		switch ($targetEditor) {
 			case 'excerpt':
-				$I->scrollTo('#postexcerpt');
+				$I->scrollTo('#postcustom');
 				break;
 			default:
 				$I->scrollTo('h1.wp-heading-inline');
