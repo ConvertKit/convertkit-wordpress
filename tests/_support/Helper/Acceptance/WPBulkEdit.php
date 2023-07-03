d85804fc24720423b7343f856489cefcb243111b
Tim Carr <tim@n7studios.co.uk>
2023-07-03 15:35:31 +0100

    Bulk Edit: Click label instead of checkbox, as label now covers the checkbox and intercepts the click

--- tests/_support/Helper/Acceptance/WPBulkEdit.php
+++ tests/_support/Helper/Acceptance/WPBulkEdit.php
@@ -69,7 +69,7 @@ class WPBulkEdit extends \Codeception\Module
 
 		// Check boxes for Post IDs.
 		foreach ($postIDs as $postID) {
-			$I->checkOption('#cb-select-' . $postID);
+			$I->click('label[for="cb-select-' . $postID . '"]');
 		}
 
 		// Select Edit from the Bulk actions dropdown.
