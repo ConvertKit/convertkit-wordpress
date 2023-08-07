0059c8ed40a1021cfd8ac83a063745fe8a63b1bf
Tim Carr <tim@n7studios.co.uk>
2023-08-07 17:18:11 +0100

    Reinstate check for PHP warnings or notices

--- tests/acceptance/integrations/WooCommerceProductFormCest.php
+++ tests/acceptance/integrations/WooCommerceProductFormCest.php
@@ -493,6 +493,9 @@ class WooCommerceProductFormCest
 			// Load Page on the frontend site.
 			$I->amOnPage('/?p=' . $productID);
 
+			// Check that no PHP warnings or notices were output.
+			$I->checkNoWarningsAndNoticesOnScreen($I);
+
 			// Confirm that one ConvertKit Form is output in the DOM.
 			// This confirms that there is only one script on the page for this form, which renders the form.
 			$I->seeNumberOfElementsInDOM('form[data-sv-form="' . $_ENV['CONVERTKIT_API_FORM_ID'] . '"]', 1);
