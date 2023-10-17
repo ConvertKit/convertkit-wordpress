1efa58c4b6c453c140a233a8e688088f5506eb59
Tim Carr <tim@n7studios.co.uk>
2023-10-17 17:52:54 +0100

    Coding standards

--- includes/blocks/class-convertkit-block-product.php
+++ includes/blocks/class-convertkit-block-product.php
@@ -237,12 +237,12 @@ class ConvertKit_Block_Product extends ConvertKit_Block {
 		return array(
 			'className'  => true,
 			'color'      => array(
-				'background' => true,
-				'text'       => true,
+				'background'                      => true,
+				'text'                            => true,
 
 				// Don't apply styles to the block editor's div element.
 				// This ensures what's rendered in the Gutenberg editor matches the frontend output for styling.
-				// See: https://github.com/WordPress/gutenberg/issues/32417
+				// See: https://github.com/WordPress/gutenberg/issues/32417.
 				'__experimentalSkipSerialization' => true,
 			),
 			'typography' => array(
