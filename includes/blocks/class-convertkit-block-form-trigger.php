f18c44efde4e970c8ed34066b8267749a3ee0177
Tim Carr <tim@n7studios.co.uk>
2023-10-17 17:25:08 +0100

    Use `__experimentalSkipSerialization`

--- includes/blocks/class-convertkit-block-form-trigger.php
+++ includes/blocks/class-convertkit-block-form-trigger.php
@@ -215,6 +215,11 @@ class ConvertKit_Block_Form_Trigger extends ConvertKit_Block {
 			'color'      => array(
 				'background' => true,
 				'text'       => true,
+
+				// Don't apply styles to the block editor's div element.
+				// This ensures what's rendered in the Gutenberg editor matches the frontend output for styling.
+				// See: https://github.com/WordPress/gutenberg/issues/32417
+				'__experimentalSkipSerialization' => true,
 			),
 			'typography' => array(
 				'fontSize' => true,
