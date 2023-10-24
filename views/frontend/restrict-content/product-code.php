c1f14fc067e3eb406a03428c6e3d2b30edd504c2
Tim Carr <tim@n7studios.co.uk>
2023-10-24 16:36:49 +0100

    Started work on verification code UI

--- views/frontend/restrict-content/product-code.php
+++ views/frontend/restrict-content/product-code.php
@@ -18,8 +18,9 @@
 	<div class="convertkit-restrict-content-actions">
 		<form class="convertkit-restrict-content-subscriber-code" action="<?php echo esc_attr( add_query_arg( array( 'convertkit_login' => 1 ), get_permalink( $post_id ) ) ); ?>" method="post">
 			<div>
-				<input type="text" name="subscriber_code" id="convertkit_subscriber_code" inputmode="numeric" pattern="[0-9]{6}" autocomplete="one-time-code" value="" placeholder="<?php esc_attr_e( 'Email Code', 'convertkit' ); ?>" />
-
+				<div id="convertkit-subscriber-code-container">
+					<input type="text" name="subscriber_code" id="convertkit_subscriber_code" inputmode="numeric" maxlength="6" pattern="[0-9]{6}" autocomplete="one-time-code" value="" />
+				</div>
 				<input type="submit" class="wp-block-button__link" value="<?php esc_html_e( 'Verify', 'convertkit' ); ?>" />
 
 				<input type="hidden" name="token" value="<?php echo esc_attr( $this->token ); ?>" />
@@ -27,5 +28,12 @@
 				<?php wp_nonce_field( 'convertkit_restrict_content_subscriber_code' ); ?>
 			</div>
 		</form>
+
+		<script>
+			const convertKitRestrictContentSubscriberCodeInput = document.querySelector( '#convertkit_subscriber_code' );
+			convertKitRestrictContentSubscriberCodeInput.addEventListener( 'input', function() {
+				convertKitRestrictContentSubscriberCodeInput.style.setProperty( '--_otp-digit', convertKitRestrictContentSubscriberCodeInput.selectionStart );
+			} );
+		</script>
 	</div>
 </div>
