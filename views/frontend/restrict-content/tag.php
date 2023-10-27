f8ec5fe646b7754d73cee5abafa4486383c298da
Tim Carr <tim@n7studios.co.uk>
2023-10-27 14:12:27 +0100

    Tags: Update email placeholder to match Products

--- views/frontend/restrict-content/tag.php
+++ views/frontend/restrict-content/tag.php
@@ -22,7 +22,7 @@
 
 		<form class="convertkit-restrict-content-login" action="<?php echo esc_attr( add_query_arg( array( 'convertkit_login' => 1 ), get_permalink( $post_id ) ) ); ?>#convertkit-restrict-content" method="post">
 			<div>
-				<input type="email" name="convertkit_email" id="convertkit_email" value="" placeholder="example@convertkit.com" />
+				<input type="email" name="convertkit_email" id="convertkit_email" value="" placeholder="<?php esc_attr_e( 'Email Address', 'convertkit' ); ?>" />
 				<input type="submit" class="wp-block-button__link wp-block-button__link" value="<?php echo esc_attr( $this->restrict_content_settings->get_by_key( 'subscribe_button_label' ) ); ?>" />
 				<input type="hidden" name="convertkit_resource_type" value="<?php echo esc_attr( $resource_type ); ?>" />
 				<input type="hidden" name="convertkit_resource_id" value="<?php echo esc_attr( $resource_id ); ?>" />
