6033a516aa44a37536fc5ea688287a61623314bb
Tim Carr <tim@n7studios.co.uk>
2023-10-30 14:34:59 +0000

    Remove unnecessary div elements; target styling for email field vs. one time code field

--- views/frontend/restrict-content/product-code.php
+++ views/frontend/restrict-content/product-code.php
@@ -17,16 +17,13 @@
 
 	<div class="convertkit-restrict-content-actions">
 		<form class="convertkit-restrict-content-subscriber-code" action="<?php echo esc_attr( add_query_arg( array( 'convertkit_login' => 1 ), get_permalink( $post_id ) ) ); ?>" method="post">
-			<div>
-				<div id="convertkit-subscriber-code-container">
-					<input type="text" name="subscriber_code" id="convertkit_subscriber_code" inputmode="numeric" maxlength="6" pattern="[0-9]{6}" autocomplete="one-time-code" value="" />
-				</div>
-				<input type="submit" class="wp-block-button__link" value="<?php esc_html_e( 'Verify', 'convertkit' ); ?>" />
-
-				<input type="hidden" name="token" value="<?php echo esc_attr( $this->token ); ?>" />
-
-				<?php wp_nonce_field( 'convertkit_restrict_content_subscriber_code' ); ?>
+			<div id="convertkit-subscriber-code-container">
+				<input type="text" name="subscriber_code" id="convertkit_subscriber_code" inputmode="numeric" maxlength="6" pattern="[0-9]{6}" autocomplete="one-time-code" value="" />
 			</div>
+
+			<input type="submit" class="wp-block-button__link" value="<?php esc_html_e( 'Verify', 'convertkit' ); ?>" />
+			<input type="hidden" name="token" value="<?php echo esc_attr( $this->token ); ?>" />
+			<?php wp_nonce_field( 'convertkit_restrict_content_subscriber_code' ); ?>
 		</form>
 
 		<script>
--- views/frontend/restrict-content/tag.php
+++ views/frontend/restrict-content/tag.php
@@ -27,12 +27,11 @@
 		</p>
 
 		<form class="convertkit-restrict-content-login" action="<?php echo esc_attr( add_query_arg( array( 'convertkit_login' => 1 ), get_permalink( $post_id ) ) ); ?>#convertkit-restrict-content" method="post">
-			<div>
+			<div id="convertkit-restrict-content-email-field">
 				<input type="email" name="convertkit_email" id="convertkit_email" value="" placeholder="<?php esc_attr_e( 'Email Address', 'convertkit' ); ?>" />
 				<input type="submit" class="wp-block-button__link wp-block-button__link" value="<?php echo esc_attr( $this->restrict_content_settings->get_by_key( 'subscribe_button_label' ) ); ?>" />
 				<input type="hidden" name="convertkit_resource_type" value="<?php echo esc_attr( $resource_type ); ?>" />
 				<input type="hidden" name="convertkit_resource_id" value="<?php echo esc_attr( $resource_id ); ?>" />
-				
 				<?php wp_nonce_field( 'convertkit_restrict_content_login' ); ?>
 			</div>
 		</form>
