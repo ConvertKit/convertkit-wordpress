0de09e304e43994c78110aeaa019ce148df2e086
Tim Carr <tim@n7studios.co.uk>
2023-10-30 16:19:45 +0000

    Move errors inline below email field for products and tags

--- views/frontend/restrict-content/product.php
+++ views/frontend/restrict-content/product.php
@@ -12,10 +12,6 @@
 ?>
 
 <div id="convertkit-restrict-content">
-	<?php
-	require 'notices.php';
-	?>
-
 	<h3><?php echo esc_html( $this->restrict_content_settings->get_by_key( 'subscribe_heading' ) ); ?></h3>
 	<p>
 		<?php
@@ -32,7 +28,7 @@
 	<p><?php echo esc_html( $this->restrict_content_settings->get_by_key( 'email_text' ) ); ?></p>
 
 	<form class="convertkit-restrict-content-login" action="<?php echo esc_attr( add_query_arg( array( 'convertkit_login' => 1 ), get_permalink( $post_id ) ) ); ?>#convertkit-restrict-content" method="post">
-		<div id="convertkit-restrict-content-email-field">
+		<div id="convertkit-restrict-content-email-field" class="<?php echo sanitize_html_class( ( is_wp_error( $error ) ? 'convertkit-restrict-content-error' : '' ) ); ?>">
 			<input type="email" name="convertkit_email" id="convertkit_email" value="" placeholder="<?php esc_attr_e( 'Email Address', 'convertkit' ); ?>" />
 			<input type="submit" class="wp-block-button__link wp-block-button__link" value="<?php echo esc_attr( $this->restrict_content_settings->get_by_key( 'email_button_label' ) ); ?>" />
 			<input type="hidden" name="convertkit_resource_type" value="<?php echo esc_attr( $resource_type ); ?>" />
@@ -40,6 +36,10 @@
 			<?php wp_nonce_field( 'convertkit_restrict_content_login' ); ?>
 		</div>
 	</form>
+
+	<?php
+	require 'notices.php';
+	?>
 
 	<small><?php echo esc_html( $this->restrict_content_settings->get_by_key( 'email_description_text' ) ); ?></small>
 </div>
--- views/frontend/restrict-content/tag.php
+++ views/frontend/restrict-content/tag.php
@@ -1,9 +1,9 @@
 <?php
 /**
- * Outputs the restricted content product message,
+ * Outputs the restricted content tag message,
  * and a form for the subscriber to enter their
- * email address if they've already subscribed
- * to the ConvertKit Product.
+ * email address to subscribe to the tag, granting
+ * them access.
  *
  * @package ConvertKit
  * @author ConvertKit
@@ -12,10 +12,6 @@
 ?>
 
 <div id="convertkit-restrict-content">
-	<?php
-	require 'notices.php';
-	?>
-
 	<div class="convertkit-restrict-content-actions">
 		<h3><?php echo esc_html( $this->restrict_content_settings->get_by_key( 'subscribe_heading_tag' ) ); ?></h3>
 		<p>
@@ -27,7 +23,7 @@
 		</p>
 
 		<form class="convertkit-restrict-content-login" action="<?php echo esc_attr( add_query_arg( array( 'convertkit_login' => 1 ), get_permalink( $post_id ) ) ); ?>#convertkit-restrict-content" method="post">
-			<div id="convertkit-restrict-content-email-field">
+			<div id="convertkit-restrict-content-email-field" class="<?php echo sanitize_html_class( ( is_wp_error( $error ) ? 'convertkit-restrict-content-error' : '' ) ); ?>">
 				<input type="email" name="convertkit_email" id="convertkit_email" value="" placeholder="<?php esc_attr_e( 'Email Address', 'convertkit' ); ?>" />
 				<input type="submit" class="wp-block-button__link wp-block-button__link" value="<?php echo esc_attr( $this->restrict_content_settings->get_by_key( 'subscribe_button_label' ) ); ?>" />
 				<input type="hidden" name="convertkit_resource_type" value="<?php echo esc_attr( $resource_type ); ?>" />
@@ -35,5 +31,9 @@
 				<?php wp_nonce_field( 'convertkit_restrict_content_login' ); ?>
 			</div>
 		</form>
+
+		<?php
+		require 'notices.php';
+		?>
 	</div>
 </div>
