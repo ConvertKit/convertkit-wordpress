7d2471de049f818ce5007e883241efb2b63f361e
Tim Carr <tim@n7studios.co.uk>
2024-02-16 14:11:52 +0000

    Add `data-no-defer` to all form scripts

--- includes/class-convertkit-resource-forms.php
+++ includes/class-convertkit-resource-forms.php
@@ -324,9 +324,10 @@ class ConvertKit_Resource_Forms extends ConvertKit_Resource {
 		}
 
 		// If here, return Form <script> embed now, as we want the inline form to display at this specific point of the content.
-		// data-jetpack-boost="ignore" is required to prevent Jetpack Boost from moving this to the footer when its
-		// "Defer Non-Essential JavaScript" setting is enabled: https://jetpack.com/support/jetpack-boost/.
-		return '<script async data-uid="' . esc_attr( $this->resources[ $id ]['uid'] ) . '" src="' . esc_url( $this->resources[ $id ]['embed_js'] ) . '" data-jetpack-boost="ignore"></script>';
+		// To prevent third party caching / performance Plugins moving this to the footer of the site, data- attributes are added:
+		// data-jetpack-boost="ignore": Ignore Jetpack Boost "Defer Non-Essential JavaScript" setting: https://jetpack.com/support/jetpack-boost/.
+		// data-no-defer="1": Ignore LiteSpeed Cache "Load JS Deferred" setting .
+		return '<script async data-uid="' . esc_attr( $this->resources[ $id ]['uid'] ) . '" src="' . esc_url( $this->resources[ $id ]['embed_js'] ) . '" data-jetpack-boost="ignore" data-no-defer="1"></script>';
 
 	}
 
