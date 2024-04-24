1936a9f035c027af2fc8d76893aabe44b621aa03
Tim Carr <tim@n7studios.co.uk>
2024-04-24 17:06:08 +0100

    Check for new ‘connect your ConvertKit account’ message

--- tests/acceptance/general/PostCest.php
+++ tests/acceptance/general/PostCest.php
@@ -44,10 +44,17 @@ class PostCest
 		$I->dontSeeElementInDOM('#wp-convertkit-form');
 
 		// Check that an expected message is displayed.
-		$I->see('To configure the ConvertKit Form / Landing Page to display on this Post, enter your ConvertKit API credentials');
+		$I->see('For the ConvertKit Plugin to function, please connect your ConvertKit account.');
 
-		// Check that a link to the Plugin Settings exists.
-		$I->seeInSource('<a href="' . $_ENV['TEST_SITE_WP_URL'] . '/wp-admin/options-general.php?page=_wp_convertkit_settings">Plugin Settings</a>');
+		// Check that a link to the OAuth auth screen exists.
+		$I->seeInSource('<a href="https://app.convertkit.com/oauth/authorize?client_id=' . $_ENV['CONVERTKIT_OAUTH_CLIENT_ID'] . '&amp;response_type=code&amp;redirect_uri=' . urlencode( $_ENV['TEST_SITE_WP_URL'] ) );
+
+		// Click the link.
+		$I->click('connect your ConvertKit account.');
+
+		// Confirm the ConvertKit hosted OAuth login screen is displayed.
+		$I->waitForElementVisible('body.sessions');
+		$I->seeInSource('oauth/authorize?client_id=' . $_ENV['CONVERTKIT_OAUTH_CLIENT_ID']);
 	}
 
 	/**
