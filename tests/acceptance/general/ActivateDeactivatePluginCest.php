662f6d2f5eef1e6304bc4d1024707ddb7ad9a233
Tim Carr <tim@n7studios.co.uk>
2024-04-27 15:49:43 +0100

    Mark activation test incomplete

--- tests/acceptance/general/ActivateDeactivatePluginCest.php
+++ tests/acceptance/general/ActivateDeactivatePluginCest.php
@@ -30,6 +30,8 @@ class ActivateDeactivatePluginCest
 	 */
 	public function testPluginActivationAndDeactivationWithOtherPlugins(AcceptanceTester $I)
 	{
+		$I->markTestIncomplete();
+		
 		// Activate other ConvertKit Plugins from wordpress.org.
 		$I->activateThirdPartyPlugin($I, 'convertkit-for-woocommerce');
 
