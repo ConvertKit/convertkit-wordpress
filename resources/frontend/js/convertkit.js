f69542cf17f197efa43155caaf1dc0d9cf893886
Aleksej <aleksejvu@gmail.com>
2024-05-31 13:42:47 +0200

    Update formatting.

--- resources/frontend/js/convertkit.js
+++ resources/frontend/js/convertkit.js
@@ -17,48 +17,59 @@
  * @param   int     post_id         WordPress Post ID
  */
 function convertKitTagSubscriber(subscriber_id, tag, post_id) {
-  if (convertkit.debug) {
-    console.log("convertKitTagSubscriber");
-    console.log(convertkit);
-    console.log(subscriber_id);
-    console.log(tag);
-    console.log(post_id);
-  }
+	if (convertkit.debug) {
+		console.log( "convertKitTagSubscriber" );
+		console.log( convertkit );
+		console.log( subscriber_id );
+		console.log( tag );
+		console.log( post_id );
+	}
 
-  fetch(convertkit.ajaxurl, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/x-www-form-urlencoded",
-    },
-    body: new URLSearchParams({
-      action: "convertkit_tag_subscriber",
-      convertkit_nonce: convertkit.nonce,
-      subscriber_id: subscriber_id,
-      tag: tag,
-      post_id: post_id,
-    }),
-  })
-    .then(function (response) {
-      if (convertkit.debug) {
-        console.log(response);
-      }
+	fetch(
+		convertkit.ajaxurl,
+		{
+			method: "POST",
+			headers: {
+				"Content-Type": "application/x-www-form-urlencoded",
+			},
+			body: new URLSearchParams(
+				{
+					action: "convertkit_tag_subscriber",
+					convertkit_nonce: convertkit.nonce,
+					subscriber_id: subscriber_id,
+					tag: tag,
+					post_id: post_id,
+				}
+			),
+		}
+	)
+	.then(
+		function (response) {
+			if (convertkit.debug) {
+				console.log( response );
+			}
 
-      return response.json();
-    })
-    .then(function (result) {
-      if (convertkit.debug) {
-        console.log(result);
-      }
+			return response.json();
+		}
+	)
+	.then(
+		function (result) {
+			if (convertkit.debug) {
+				console.log( result );
+			}
 
-      convertKitRemoveSubscriberIDFromURL(window.location.href);
-    })
-    .catch(function (error) {
-      if (convertkit.debug) {
-        console.error(error);
-      }
+			convertKitRemoveSubscriberIDFromURL( window.location.href );
+		}
+	)
+	.catch(
+		function (error) {
+			if (convertkit.debug) {
+				console.error( error );
+			}
 
-      convertKitRemoveSubscriberIDFromURL(window.location.href);
-    });
+			convertKitRemoveSubscriberIDFromURL( window.location.href );
+		}
+	);
 }
 
 /**
@@ -75,49 +86,63 @@ function convertKitTagSubscriber(subscriber_id, tag, post_id) {
  * @param   int  id   Subscriber ID
  */
 function convertStoreSubscriberIDInCookie(subscriber_id) {
-  if (convertkit.debug) {
-    console.log("convertStoreSubscriberIDInCookie");
-    console.log(subscriber_id);
-  }
+	if (convertkit.debug) {
+		console.log( "convertStoreSubscriberIDInCookie" );
+		console.log( subscriber_id );
+	}
 
-  fetch(convertkit.ajaxurl, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/x-www-form-urlencoded",
-    },
-    body: new URLSearchParams({
-      action: "convertkit_store_subscriber_id_in_cookie",
-      convertkit_nonce: convertkit.nonce,
-      subscriber_id: subscriber_id,
-    }),
-  })
-    .then(function (response) {
-      if (convertkit.debug) {
-        console.log(response);
-      }
+	fetch(
+		convertkit.ajaxurl,
+		{
+			method: "POST",
+			headers: {
+				"Content-Type": "application/x-www-form-urlencoded",
+			},
+			body: new URLSearchParams(
+				{
+					action: "convertkit_store_subscriber_id_in_cookie",
+					convertkit_nonce: convertkit.nonce,
+					subscriber_id: subscriber_id,
+				}
+			),
+		}
+	)
+	.then(
+		function (response) {
+			if (convertkit.debug) {
+				console.log( response );
+			}
 
-      return response.json();
-    })
-    .then(function (result) {
-      if (convertkit.debug) {
-        console.log(result);
-      }
+			return response.json();
+		}
+	)
+	.then(
+		function (result) {
+			if (convertkit.debug) {
+				console.log( result );
+			}
 
-      if (result.success) {
-        convertKitEmitCustomEvent("convert_kit_user_subscribed", {
-          subscriber_id,
-        });
-      }
+			if (result.success) {
+				convertKitEmitCustomEvent(
+					"convert_kit_user_subscribed",
+					{
+						subscriber_id,
+					}
+				);
+			}
 
-      convertKitRemoveSubscriberIDFromURL(window.location.href);
-    })
-    .catch(function (error) {
-      if (convertkit.debug) {
-        console.error(error);
-      }
+			convertKitRemoveSubscriberIDFromURL( window.location.href );
+		}
+	)
+	.catch(
+		function (error) {
+			if (convertkit.debug) {
+				console.error( error );
+			}
 
-      convertKitRemoveSubscriberIDFromURL(window.location.href);
-    });
+			convertKitRemoveSubscriberIDFromURL( window.location.href );
+		}
+	);
 }
 
 /**
@@ -134,37 +159,48 @@ function convertStoreSubscriberIDInCookie(subscriber_id) {
  * @param   string  emailAddress   Email Address
  */
 function convertStoreSubscriberEmailAsIDInCookie(emailAddress) {
-  if (convertkit.debug) {
-    console.log("convertStoreSubscriberEmailAsIDInCookie");
-    console.log(emailAddress);
-  }
+	if (convertkit.debug) {
+		console.log( "convertStoreSubscriberEmailAsIDInCookie" );
+		console.log( emailAddress );
+	}
 
-  fetch(convertkit.ajaxurl, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/x-www-form-urlencoded",
-    },
-    body: new URLSearchParams({
-      action: "convertkit_store_subscriber_email_as_id_in_cookie",
-      convertkit_nonce: convertkit.nonce,
-      email: emailAddress,
-    }),
-  })
-    .then(function (response) {
-      if (convertkit.debug) {
-        console.log(response);
-      }
-    })
-    .then(function (result) {
-      if (convertkit.debug) {
-        console.log(result);
-      }
-    })
-    .catch(function (error) {
-      if (convertkit.debug) {
-        console.error(error);
-      }
-    });
+	fetch(
+		convertkit.ajaxurl,
+		{
+			method: "POST",
+			headers: {
+				"Content-Type": "application/x-www-form-urlencoded",
+			},
+			body: new URLSearchParams(
+				{
+					action: "convertkit_store_subscriber_email_as_id_in_cookie",
+					convertkit_nonce: convertkit.nonce,
+					email: emailAddress,
+				}
+			),
+		}
+	)
+	.then(
+		function (response) {
+			if (convertkit.debug) {
+				console.log( response );
+			}
+		}
+	)
+	.then(
+		function (result) {
+			if (convertkit.debug) {
+				console.log( result );
+			}
+		}
+	)
+	.catch(
+		function (error) {
+			if (convertkit.debug) {
+				console.error( error );
+			}
+		}
+	);
 }
 
 /**
@@ -178,11 +214,11 @@ function convertStoreSubscriberEmailAsIDInCookie(emailAddress) {
  * @param url
  */
 function convertKitRemoveSubscriberIDFromURL(url) {
-  var clean_url = url.substring(0, url.indexOf("?ck_subscriber_id"));
-  var title = document.getElementsByTagName("title")[0].innerHTML;
-  if (clean_url) {
-    window.history.pushState(null, title, clean_url);
-  }
+	var clean_url = url.substring( 0, url.indexOf( "?ck_subscriber_id" ) );
+	var title     = document.getElementsByTagName( "title" )[0].innerHTML;
+	if (clean_url) {
+		window.history.pushState( null, title, clean_url );
+	}
 }
 
 /**
@@ -193,12 +229,12 @@ function convertKitRemoveSubscriberIDFromURL(url) {
  * @param   int     milliseconds
  */
 function convertKitSleep(milliseconds) {
-  var start = new Date().getTime();
-  for (var i = 0; i < 1e7; i++) {
-    if (new Date().getTime() - start > milliseconds) {
-      break;
-    }
-  }
+	var start = new Date().getTime();
+	for (var i = 0; i < 1e7; i++) {
+		if (new Date().getTime() - start > milliseconds) {
+			break;
+		}
+	}
 }
 
 /**
@@ -213,69 +249,75 @@ function convertKitSleep(milliseconds) {
  * @param {Object} [detail={}] - Optional detail data to include with the event.
  */
 function convertKitEmitCustomEvent(eventName, detail) {
-  const event = new CustomEvent(eventName, { detail });
-  document.dispatchEvent(event);
+	const event = new CustomEvent( eventName, { detail } );
+	document.dispatchEvent( event );
 }
 
 /**
  * Register events
  */
-document.addEventListener("DOMContentLoaded", function () {
-  if (convertkit.subscriber_id > 0 && convertkit.tag && convertkit.post_id) {
-    // If the user can be detected as a ConvertKit Subscriber (i.e. their Subscriber ID is in a cookie or the URL),
-    // and the Page/Post they are viewing has a Tag specified, subscribe them to the tag.
-    convertKitTagSubscriber(
-      convertkit.subscriber_id,
-      convertkit.tag,
-      convertkit.post_id
-    );
-  } else if (convertkit.subscriber_id > 0) {
-    // If the user can be detected as a ConvertKit Subscriber (i.e. their Subscriber ID is in a cookie or the URL),
-    // update the cookie now.
-    convertStoreSubscriberIDInCookie(convertkit.subscriber_id);
-  }
+document.addEventListener(
+	"DOMContentLoaded",
+	function () {
+		if (convertkit.subscriber_id > 0 && convertkit.tag && convertkit.post_id) {
+			// If the user can be detected as a ConvertKit Subscriber (i.e. their Subscriber ID is in a cookie or the URL),
+			// and the Page/Post they are viewing has a Tag specified, subscribe them to the tag.
+			convertKitTagSubscriber(
+				convertkit.subscriber_id,
+				convertkit.tag,
+				convertkit.post_id
+			);
+		} else if (convertkit.subscriber_id > 0) {
+			// If the user can be detected as a ConvertKit Subscriber (i.e. their Subscriber ID is in a cookie or the URL),
+			// update the cookie now.
+			convertStoreSubscriberIDInCookie( convertkit.subscriber_id );
+		}
 
-  // Store subscriber ID as a cookie from the email address used when a ConvertKit Form is submitted.
-  document.addEventListener("click", function (e) {
-    // Check if the form submit button was clicked, or the span element was clicked and its parent is the form submit button.
-    if (
-      !e.target.matches(".formkit-submit") &&
-      !e.target.parentElement.matches(".formkit-submit")
-    ) {
-      if (convertkit.debug) {
-        console.log("not a ck form");
-      }
+		// Store subscriber ID as a cookie from the email address used when a ConvertKit Form is submitted.
+		document.addEventListener(
+			"click",
+			function (e) {
+				// Check if the form submit button was clicked, or the span element was clicked and its parent is the form submit button.
+				if (
+				! e.target.matches( ".formkit-submit" ) &&
+				! e.target.parentElement.matches( ".formkit-submit" )
+				) {
+					if (convertkit.debug) {
+							console.log( "not a ck form" );
+					}
 
-      return;
-    }
+					return;
+				}
 
-    // Get email address.
-    let emailAddress = document.querySelector(
-      'input[name="email_address"]'
-    ).value;
+				// Get email address.
+				let emailAddress = document.querySelector(
+					'input[name="email_address"]'
+				).value;
 
-    // If the email address is empty, don't attempt to get the subscriber ID by email.
-    if (!emailAddress.length) {
-      if (convertkit.debug) {
-        console.log("email empty");
-      }
+				// If the email address is empty, don't attempt to get the subscriber ID by email.
+				if ( ! emailAddress.length) {
+					if (convertkit.debug) {
+						console.log( "email empty" );
+					}
 
-      return;
-    }
+					return;
+				}
 
-    // If the email address is invalid, don't attempt to get the subscriber ID by email.
-    var validator =
-      /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    if (!validator.test(emailAddress.toLowerCase())) {
-      if (convertkit.debug) {
-        console.log("email not an email address");
-      }
+				// If the email address is invalid, don't attempt to get the subscriber ID by email.
+				var validator =
+				/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+				if ( ! validator.test( emailAddress.toLowerCase() )) {
+					if (convertkit.debug) {
+						console.log( "email not an email address" );
+					}
 
-      return;
-    }
+					return;
+				}
 
-    // Wait a moment before sending the AJAX request.
-    convertKitSleep(2000);
-    convertStoreSubscriberEmailAsIDInCookie(emailAddress);
-  });
-});
+				// Wait a moment before sending the AJAX request.
+				convertKitSleep( 2000 );
+				convertStoreSubscriberEmailAsIDInCookie( emailAddress );
+			}
+		);
+	}
+);
