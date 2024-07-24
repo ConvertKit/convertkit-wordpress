ac7d58974cc17e0b056571054f61195462d85587
Tim Carr <tim@n7studios.co.uk>
2024-07-24 12:29:46 +0800

    Fix frontend logic

--- includes/integrations/forminator/class-convertkit-forminator.php
+++ includes/integrations/forminator/class-convertkit-forminator.php
@@ -81,7 +81,7 @@ class ConvertKit_Forminator {
 		// We deliberately use the entry's form ID, as $form_id for a Quiz will point to a lead generation form, which
 		// has a different Form ID.
 		$forminator_settings = new ConvertKit_Forminator_Settings();
-		$convertkit_form_id  = $forminator_settings->get_convertkit_form_id_by_forminator_form_id( $entry->form_id );
+		$convertkit_subscribe_setting  = $forminator_settings->get_convertkit_subscribe_setting_by_forminator_form_id( $entry->form_id );
 
 		// If no ConvertKit Form is mapped to this Forminator Form, bail.
 		if ( ! $convertkit_form_id ) {
